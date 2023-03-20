8eb4c35cd655bb4ee4a94c217abae353fc37c7cf
kuberlog <collinalexbell@gmail.com>
2023-03-20 05:09:15 +0000

    init src

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * TA1-1
+ * 
+ * Tree Cybernetics
+ *  Arm
+ *   1st Model
+ *     1st Iteration
+ */
+
+#include "Arduino.h"
+#include <Servo.h>
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+int main_lever_pin = 2;
+Servo servo;
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  servo.attach(main_lever_pin);
+}
+
+int i = 0;
+int delta = 1;
+void loop()
+{
+  i = i + delta;
+  if(i>180 || i < 0) {
+    delta = delta * -1;
+  }
+  servo.write(i);
+}
