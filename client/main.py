70773b53d4322f11d308f6a779fdc039686d4294
kuberlog <collinalexbell@gmail.com>
2023-03-21 19:48:14 -0400

    init commands, coms, joint, and model

--- client/main.py
+++ client/main.py
@@ -1,6 +1,12 @@
+from coms import Coms
 from curses import wrapper
+import commands
+from joint import Joint
 
 def main(stdscr):
+    main_arm = Joint()
+    command = commands.move_joint_to_pos(main_arm, 150, 1)
+    coms = Coms("/dev/ttyUSB0")
     stdscr.clear()
     stdscr.addstr(0, 0, "Tree Arm v1 i1")
 
